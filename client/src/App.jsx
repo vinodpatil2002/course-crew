4396be2565b1546d9e4f188d43b40767d36f7e36
vinodpatil2002 <vinodnspatil@gmail.com>
2024-10-07 20:48:07 +0530

    Update react-router-dom to version 6.26.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import CreateCourse from './pages/CreateCourse'
+import UpdateCourse from './pages/UpdateCourse'
+import CoursePage from './pages/CoursePage'
+import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+
+
+import Search from './pages/Search'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute/>}>
+        <Route path='/create-post' element={<CreateCourse/>}/>
+        <Route path='/update-post/:postId' element={<UpdateCourse/>}/>
+        </Route>
+        <Route path='/course/:courseSlug' element={<CoursePage/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
